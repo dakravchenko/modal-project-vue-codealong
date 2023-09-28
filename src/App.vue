b2683b788643f609531270060dc70ce18c56a823
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-09-28 09:24:06 +0200

    scoped css, prps, emited custom events, slots, teleports

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,60 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
-  <!-- <Modal/> -->
+  <p>Welcome...</p>
+
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja Givaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </teleport>  
+
+  <teleport to=".modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the Newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+
+  <button @click.alt="toggleModal">open modal (alt click)</button>
+  <button @click="toggleModalTwo">open modal 2</button>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
+// challenge
+//   - create an extra button to open a different modal
+//   - use the same modal component but pass in a different template (slot)
+//   - use a different method (e.g. toggleModalTwo) and data (e.g. showModalTwo)
+
+import Modal from './components/Modal'
 
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
-      title: 'My first vue app'
+      title: 'My First Vue App!',
+      showModal: false,
+      showModalTwo: false,
     }
   },
   methods: {
-    handleClick(){
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
-  },
-  components: {Modal}
+  }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -35,5 +62,18 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+button {
+  background: #bbb;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  margin: 10px;
+  font-size: 16px;
+  color: #333;
+}
 </style>
