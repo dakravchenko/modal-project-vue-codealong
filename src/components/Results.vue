cd92c89f976581c82f3bc1e4f2aba505080c9879
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-09-28 11:27:16 +0200

    timer project complete, lc hooks

---
+++ src/components/Results.vue
@@ -0,0 +1,17 @@
+<template>
+  <p >Reaction time: {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+    props: ['score']
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App2 from './App2.vue'
 import './assets/global.css'
 
-createApp(App).mount('#app')
+createApp(App2).mount('#app')
